8ba62c7b21864f972b65428c6c77fb595080d1d5
Rohit Yadav <rohitron0609@gmail.com>
2021-04-15 12:58:41 +0530

    mongoose depreceted warning removed

--- config/db.js
+++ config/db.js
@@ -5,7 +5,11 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
     try{
-        await mongoose.connect(db);
+        await mongoose.connect(db, {
+            useNewUrlParser : true,
+            useUnifiedTopology : true
+        });
+
         console.log('mongoose connected');
 
     }

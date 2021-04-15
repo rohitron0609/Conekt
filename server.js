8c048bc8ac39b3ae31d87d011d673cc208376bee
Rohit Yadav <rohitron0609@gmail.com>
2021-04-15 13:09:47 +0530

    Webpage routes added

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ connectDB();
 
 app.get('/', (req,res) => res.send('API Running'));
 
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profiles',require('./routes/api/profiles'));
+app.use('/api/posts',require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log('server started on port ' + PORT));

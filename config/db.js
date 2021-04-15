a719d01436bf3d7d0adf2d4f55246d964b8cd382
Rohit Yadav <rohitron0609@gmail.com>
2021-04-15 12:51:35 +0530

    Mongoose connection

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const { compareSync } = require('bcryptjs');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try{
+        await mongoose.connect(db);
+        console.log('mongoose connected');
+
+    }
+    catch(err){
+        console.log(err.message);
+
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;

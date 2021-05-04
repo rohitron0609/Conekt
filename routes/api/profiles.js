4b6fa3be2bc046f5b69d5e1feb377761669beadf
Rohit Yadav <rohitron0609@gmail.com>
2021-05-04 22:29:51 +0530

    Add and delete experience

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -184,5 +184,17 @@ async ( req, res) => {
     }
 });
 
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    
+    const profile = await Profile.findOne({ user : req.user.id });
+
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex,1);
+
+    await profile.save();
+
+    res.json(profile);
+});
 
 module.exports = router;

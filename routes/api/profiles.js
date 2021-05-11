b5bddf08ebc8ff6d8871f2ab7bfd85939eed0143
Rohit Yadav <rohitron0609@gmail.com>
2021-05-11 18:17:40 +0530

    Get Github repos

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const user = require('../../models/User');
 const { check, validationResult } = require('express-validator');
 const { compareSync } = require('bcryptjs');
+const { response } = require('express');
 
 
 //Retrieve user profile
@@ -258,5 +261,34 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 
     res.json(profile);
 });
+
+
+//Retrieve Github repos for profiles
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: {'user-agent':'node.js'}
+        }; 
+
+        request(options, (error, response, body) => {
+            if(error){
+                console.error(error);
+            }
+
+            if(response.statusCode !== 200 ){
+                return res.status(404).json({ msg: 'User not found' });
+            }
+
+            res.json(JSON.parse(body));
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 
 module.exports = router;

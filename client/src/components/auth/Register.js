7354c813fc5e0fad2c9266b17d64b3331a64ae08
Rohit Yadav <rohitron0609@gmail.com>
2021-05-16 09:40:46 +0530

    Completed Register,js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,12 +13,12 @@ export const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      console.log('success');
     }
   };
 
